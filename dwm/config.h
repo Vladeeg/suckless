a9e9417351178c75e7f819dcdfe9cfa16e5d261e
Marchenko Vladislav <vladoseeg@gmail.com>
2019-07-10 14:37:24 +0600

    Add dmenu-logout script

--- dwm/config.h
+++ dwm/config.h
@@ -64,6 +64,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *logoutprompt[]  = { "dmenu-logout", NULL };
 static const char *volumeupcmd[]  = { "amixer", "set", "Master", "5%+", NULL };
 static const char *volumedowncmd[]  = { "amixer", "set", "Master", "5%-", NULL };
 
@@ -105,7 +106,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = logoutprompt} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
 /* button definitions */
